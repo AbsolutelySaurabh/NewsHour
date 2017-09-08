792919f5a6b3d85e30ad19ca0c5e704e30de1e3a
Saurabh Singh <absolutelysaurabh03@gmail.com>
2017-09-08 21:08:01 +0530

    Update Config.java

--- app/src/main/java/com/appsomniac/newshour/Config/Config.java
+++ app/src/main/java/com/appsomniac/newshour/Config/Config.java
@@ -8,42 +8,6 @@ public class Config {
 
     public static String PLAYSTORE_LINK = "https://play.google.com/store/apps/details?id=com.appsomniac.newshour&hl=en";
 
-    public static String API_KEY = "0cd429a696954c2b837a14efc187a904";
-
-    public static String TOI_URL = "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=";
-
-    public static String HINDU_URL = "https://newsapi.org/v1/articles?source=the-hindu&sortBy=top&apiKey=";
-
-    public static String CNN_URL = "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=";
-
-    public static String TECHCRUNCH_URL = " https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=";
-
-    public static String MTVNEWS_URL = "https://newsapi.org/v1/articles?source=mtv-news&sortBy=top&apiKey=";
-
-    public static String FOXSPORTS_URL = "https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=";
-
-    public static String FINANCIALTIMES_URL = "https://newsapi.org/v1/articles?source=financial-times&sortBy=top&apiKey=";
-
-    public static String ESPN_URL = "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=";
-
-    public static String HACKERNEWS_URL = "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=";
-
-    public static String MASHABLE_URL = " https://newsapi.org/v1/articles?source=mashable&sortBy=top&apiKey=";
-
-    public static String TECHRADAR_URL = "https://newsapi.org/v1/articles?source=techradar&sortBy=top&apiKey=";
-
-    public static String THEGUARDIAN_URL = "https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=top&apiKey=";
-
-    public static String THETELEGRAPH_URL = " https://newsapi.org/v1/articles?source=the-telegraph&sortBy=top&apiKey=";
-
-    public static String TIME_URL = " https://newsapi.org/v1/articles?source=time&sortBy=top&apiKey=";
-
-    public static String BUZZ_FEEd_URL = " https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=";
-
-    public static String BUSINESS_INSIDER_URL = "https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=";
-
-    public static String NAT_GEO_URL = "https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=";
-
-    public static String RECODE_URL = " https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=";
+    // URLs 
 
 }
