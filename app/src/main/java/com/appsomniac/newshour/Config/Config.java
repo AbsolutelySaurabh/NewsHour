4d13b7019bbc3cd040895467fdb1f5b3a935c27f
Saurabh Singh <absolutelysaurabh03@gmail.com>
2017-09-08 21:12:31 +0530

    Update Config.java

--- app/src/main/java/com/appsomniac/newshour/Config/Config.java
+++ app/src/main/java/com/appsomniac/newshour/Config/Config.java
@@ -6,8 +6,49 @@ package com.appsomniac.newshour.Config;
 
 public class Config {
 
-    public static String PLAYSTORE_LINK = "https://play.google.com/store/apps/details?id=com.appsomniac.newshour&hl=en";
-
-    // URLs 
+   
+      public static String PLAYSTORE_LINK = "https://play.google.com/store/apps/details?id=com.appsomniac.newshour&hl=en";
+  
+ -    public static String API_KEY = "";
+ -
+ -    public static String TOI_URL = "";
+ -
+ -    public static String HINDU_URL = "";
+ -
+ -    public static String CNN_URL = "=";
+ -
+ -    public static String TECHCRUNCH_URL = "";
+ -
+ -    public static String MTVNEWS_URL = "";
+ -
+ -    public static String FOXSPORTS_URL = "";
+ -
+ -    public static String FINANCIALTIMES_URL = ""
+ -
+ -    public static String ESPN_URL = "";
+ -
+ -    public static String HACKERNEWS_URL = "";
+ -
+ -    public static String MASHABLE_URL = " ";
+ -
+ -    public static String TECHRADAR_URL = "";
+ -
+ -    public static String THEGUARDIAN_URL = "";
+ -
+ -    public static String THETELEGRAPH_URL = " ";
+ -
+ -    public static String TIME_URL = ""
+ -
+ -    public static String BUZZ_FEEd_URL = "";
+ -
+ -    public static String BUSINESS_INSIDER_URL = "";
+ -
+ -    public static String NAT_GEO_URL = "";
+ -
+ -    public static String RECODE_URL = "";
+ +    // URLs 
+  
+  }
+    
 
 }
